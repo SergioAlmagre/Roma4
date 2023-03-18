5a106ef36559383c5aaf10e1131df587750ea4bc
ralmodovarl01 <ramonalmodovar06@hotmail.com>
2023-03-18 20:42:20 +0100

    Creacion de la clase Auxilia con sus respectivas funciones

---
+++ src/main/kotlin/Auxilia.java
@@ -0,0 +1,13 @@
+public class Auxilia :Tropa{
+    constructor(numHombres: Int) : super(numHombres)
+
+    override fun diezmar(): String {
+        return "Vergonzorum me daria"
+    }
+
+
+
+    override fun toString(): String {
+        return "Auxilia()"
+    }
+}
